8f93b41976a3632b1a39fcc6d9d8bb02e6f32b96
Edilson <edilsoncichon@hotmail.com>
2018-07-03 09:48:19 -0300

    Layout adapted for mobile | Actions add/remove frequency basic implementation

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,37 @@
   export default {
     data () {
       return {
-        clipped: false,
-        drawer: true,
-        fixed: false,
-        itemsMenu: [
-          { icon: 'bubble_chart', title: 'Commons Words' }
-        ],
-        miniVariant: false,
-        right: true,
-        rightDrawer: false,
         title: 'Commons Words',
 
         items: [
-          { icon: 'font_download', iconClass: 'grey lighten-1 white--text', title: 'Photos', subtitle: 'Jan 9, 2014' },
-          { icon: 'font_download', iconClass: 'grey lighten-1 white--text', title: 'Recipes', subtitle: 'Jan 17, 2014' },
-          { icon: 'font_download', iconClass: 'grey lighten-1 white--text', title: 'Work', subtitle: 'Jan 28, 2014' }
+          { icon: 'P', iconClass: 'grey lighten-1 white--text', title: 'Photos', subtitle: 'Jan 9, 2014', frequency: 9 },
+          { icon: 'R', iconClass: 'grey lighten-1 white--text', title: 'Recipes', subtitle: 'Jan 17, 2014', frequency: 7 },
+          { icon: 'W', iconClass: 'grey lighten-1 white--text', title: 'Work', subtitle: 'Jan 28, 2014', frequency: 5 }
         ],
         items2: [
           { icon: 'font_download', iconClass: 'blue white--text', title: 'Vacation itinerary', subtitle: 'Jan 20, 2014' },
           { icon: 'font_download', iconClass: 'amber white--text', title: 'Kitchen remodel', subtitle: 'Jan 10, 2014' }
         ]
+      }
+    },
+
+    computed: {
+      copyright () {
+        return `${new Date().getFullYear()} - Edilson Cichon`
+      }
+    },
+
+    methods: {
+      alert (message) {
+        window.alert(message)
+      },
+
+      addFrequency (item) {
+        item.frequency++
+      },
+
+      removeFrequency (item) {
+        item.frequency > 0 && item.frequency--
       }
     }
   }
@@ -29,69 +40,68 @@
 
 <template>
   <v-app>
-    <v-navigation-drawer
-      fixed
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      v-model="drawer"
-      app
-    >
-      <v-list>
-        <v-list-tile
-          value="true"
-          v-for="(item, i) in itemsMenu"
-          :key="i"
-        >
-          <v-list-tile-action>
-            <v-icon v-html="item.icon"></v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar fixed app :clipped-left="clipped">
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>remove</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title"></v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>menu</v-icon>
-      </v-btn>
-    </v-toolbar>
     <v-content>
-      <v-container fluid>
         <v-slide-y-transition mode="out-in">
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-card>
+                <v-toolbar color="light-blue" dark>
+                  <v-toolbar-side-icon></v-toolbar-side-icon>
+
+                  <v-toolbar-title>{{ title }}</v-toolbar-title>
+
+                  <v-spacer></v-spacer>
+
+                  <v-btn icon>
+                    <v-icon>search</v-icon>
+                  </v-btn>
+                </v-toolbar>
+
+                <v-list two-line subheader>
+                  <v-subheader inset>Most frequenty</v-subheader>
+
+                  <v-list-tile
+                    v-for="item in items"
+                    :key="item.title"
+                    avatar
+                    @click=""
+                  >
+                    <v-list-tile-avatar>
+                      <v-icon :class="[item.iconClass]">{{ item.icon }}</v-icon>
+                    </v-list-tile-avatar>
+
+                    <v-list-tile-content>
+                      <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                      <v-list-tile-sub-title>{{ item.subtitle }}</v-list-tile-sub-title>
+                    </v-list-tile-content>
+
+                    <v-list-tile-action>
+                      <v-btn icon @click="removeFrequency(item)">
+                        <v-icon>remove_circle_outline</v-icon>
+                      </v-btn>
+                    </v-list-tile-action>
+
+                    <span class="ml-4">{{ item.frequency }}</span>
+
+                    <v-list-tile-action>
+                      <v-btn icon @click="addFrequency(item)">
+                        <v-icon>add_circle_outline</v-icon>
+                      </v-btn>
+                    </v-list-tile-action>
+                  </v-list-tile>
+
+                  <v-divider inset></v-divider>
+
+                  <v-subheader inset>Less frequenty</v-subheader>
+                </v-list>
+              </v-card>
+            </v-flex>
+          </v-layout>
         </v-slide-y-transition>
-      </v-container>
     </v-content>
-    <v-navigation-drawer
-      temporary
-      :right="right"
-      v-model="rightDrawer"
-      fixed
-    >
-      <v-list>
-        <v-list-tile @click.native="right = !right">
-          <v-list-tile-action>
-            <v-icon>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
+    <v-footer :fixed="false" app>
       <v-spacer></v-spacer>
-      <span>&copy; 2018 - Edilson Cichon</span>
+      <span class="mr-3">&copy {{ copyright }}</span>
     </v-footer>
   </v-app>
 </template>
